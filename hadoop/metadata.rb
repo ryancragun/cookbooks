4015bacd1f53ae32c11409d2344c3c13834f8161
Ryan Cragun <ryan@rightscale.com>
2011-01-13 12:06:15 -0800

    removed doc from hadoop

--- hadoop/metadata.rb
+++ hadoop/metadata.rb
@@ -7,13 +7,11 @@ version           "0.8.1"
 depends           "java"
 recipe            "hadoop::default", "Installs Hadoop"
 recipe            "hadoop::hive", ""
-recipe            "hadoop::doc", ""
 recipe            "hadoop::conf_pseudo", ""
 
 
 recipe "hadoop", "Installs hadoop from Cloudera's repo"
 recipe "hadoop::conf_pseudo", "Installs hadoop-conf-pseudo and enables hadoop services"
-recipe "hadoop::doc", "Installs hadoop documentation"
 recipe "hadoop::hive", "Installs hadoop's hive package"
 
 %w{ debian ubuntu }.each do |os|
